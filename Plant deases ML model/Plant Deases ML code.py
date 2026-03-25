6900319dc0c20afacc9144801b2ca9142d93eea2
Aryan289-coder <153213273+Aryan289-coder@users.noreply.github.com>
2026-03-26 00:18:14 +0530

    Add files via upload

---
+++ Plant deases ML model/Plant Deases ML code.py
@@ -0,0 +1,187 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt 
+import pandas as pd
+import seaborn as sns
+
+# data preprocessing 
+# tranning image preprocessing 
+
+tf.compat.v1.disable_eager_execution()
+
+training_set = tf.keras.utils.image_dataset_from_directory(
+    'train',
+    labels="inferred",
+    label_mode="categorical",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(128, 128),
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False,
+    pad_to_aspect_ratio=False,
+    data_format=None,
+)
+
+
+# validation image preprocessing 
+
+validation_set = tf.keras.utils.image_dataset_from_directory(
+    'valid',
+    labels="inferred",
+    label_mode="categorical",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(128, 128),
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False,
+    pad_to_aspect_ratio=False,
+    data_format=None,
+) 
+
+# cheking mate 
+training_set
+
+#check karva mate data setw
+# modified code 
+
+for x,y in training_set:
+    print(x,x.shape)
+    print(y,y.shape)
+    break
+
+# building model     
+
+from tensorflow.keras.layers import Dense,Conv2D,MaxPool2D,Flatten,Dropout 
+from tensorflow.keras.models import Sequential
+
+model = Sequential()
+
+#buliding convlution  layer 
+model.add (Conv2D(filters=32 , kernel_size=3, padding='same', activation= 'relu', input_shape=[128,128,3]))
+model.add (Conv2D(filters=32 , kernel_size=3, activation= 'relu'))
+model.add (MaxPool2D(pool_size=2 , strides=2))
+ 
+model.add (Conv2D(filters=64 , kernel_size=3, padding='same', activation= 'relu'))
+model.add (Conv2D(filters=64 , kernel_size=3, activation= 'relu'))
+model.add (Conv2D(filters=128 , kernel_size=3, padding='same', acivation= 'relu'))
+model.add (Conv2D(filters=128 , kernel_size=3, activation= 'relu'))
+model.add (MaxPool2D(pool_size=2 , strides=2))
+
+model.add (Conv2D(filters=256 , kernel_size=3, padding='same', activation= 'relu'))
+model.add (Conv2D(filters=256 , kernel_size=3, activation= 'relu'))
+model.add (MaxPool2D(pool_size=2 , strides=2))
+
+model.add (Conv2D(filters=512 , kernel_size=3, padding='same', activation= 'relu'))
+model.add (Conv2D(filters=512 , kernel_size=3, activation= 'relu'))
+model.add (MaxPool2D(pool_size=2 , strides=2))
+
+model.add(Dropout(0.25))
+model.add(Flatten()) 
+
+model.add(Dense(units=1500 , activation='relu'))
+model.add(Dropout(0.40))
+
+# output layer
+model.add(Dense(unitss=38 , activation ='softmax'))
+
+# compiling model 
+model.compile(optimizer=tf.keras.optimizers.Adam(
+    learning_rate=0.0001), loss='categorical_crossentropy' , metrics= ['accuracy'])
+model.summary()
+
+
+#traning model
+training_history=model.fit(x=training_set,validation_data=validation_set, epochs=3)
+
+#  model evaluation training set
+train_loss, train_acc = model.evaluate(training_set)  
+
+print(train_loss,train_acc)   
+
+#model on validation set 
+val_loss,val_acc = model.evaluate(validation_set)
+
+print(val_acc,val_loss)
+
+#saving model
+model.save("trained_model.keras")
+training_history.history
+
+#recording
+import json
+with open("training_hist.json" , "w") as f:
+    json.dump(training_history, history, f)
+
+training_history.history['val_accuracy']
+# training_history.history['val_accuaracy']
+#accuracy visualization
+
+epochs = [i for i in range(1,11)]
+plt.plot(epochs,training_history.history['accuracy'] , color = 'red' , label='training accuracy')
+plt.plot(epochs,training_history.history['val_accuracy'] , color = 'blue' , label='validation accuracy')
+plt.xlabel("No. of Epochs")
+plt.ylabel("Accuracy Result")
+plt.title("Visualization of Accuracy Result")
+plt.legend()
+plt.show()
+
+# vid 8
+
+class_name = validation_set.class_names
+class_name
+
+test_set = tf.keras.utils.image_dataset_from_directory(
+    'valid',
+    labels="inferred",
+    label_mode="categorical",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(128, 128),
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False,
+    pad_to_aspect_ratio=False,
+    data_format=None,
+)
+
+y_pred = model.predict(test_set)
+y_pred, y_pred.shape
+
+predict_categories = tf.argmax(y_pred, axis = 1)
+predict_categories
+
+true_categories = tf.concat([y for x,y in test_set], axis = 0)
+true_categories
+
+Y_true = tf.argmax(true_categories, axis = 1)
+Y_true
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(classification_report(Y_true, predict_categories, target_names=class_name))
+
+cm = confusion_matrix(Y_true,predict_categories)
+cm
+
+plt.figure(figsize=(40, 40))
+sns.heatmap(cm,annot=True,annot_kws={"size": 10})
+
+plt.xlabel('Predicted Class',fontsize = 20)
+plt.ylabel('Actual Class',fontsize = 20)
+plt.title('Plant Disease Prediction Confusion Matrix',fontsize = 25)
+plt.show()
